9793f5fb516906c2f853c58669d153635edb53a4
sofianelamine772-design <sofianelamine772@gmail.com>
2025-12-18 18:52:25 +0100

    d

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import { HelmetProvider } from 'react-helmet-async'
 import Header from './components/Header.jsx'
 import { AppRoutes } from './routes/index.jsx'
 
 export default function App() {
   return (
-    <>
-      <Header />
-      <main>
-        <AppRoutes />
-      </main>
-    </>
+    <HelmetProvider>
+      <div className="app">
+        <Header />
+        <main>
+          <AppRoutes />
+        </main>
+      </div>
+    </HelmetProvider>
   )
 }
